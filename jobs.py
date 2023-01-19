2cee5aef4524fbc2660fa7fb832e3471909ce5bd
Sai Karthik <110590614+sai14karthik@users.noreply.github.com>
2023-01-19 10:42:11 +0530

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -5,18 +5,21 @@ soup=BeautifulSoup(r.content,'html.parser')
 jobs=soup.find_all('h3',class_="joblist-comp-name")
 skills=soup.find_all('ul',class_="list-job-dtl clearfix")
 locations=soup.find_all('ul',class_="top-jd-dtl clearfix")
+info=soup.header.h2.a['href']
 j=[]
 s=[]
 l=[]
 for job in jobs:
-    j.append(job.text)
-    
+    j.append(job.text.replace(" ",''))
 for skill in skills:
-    s.append(skill.text)
+    s.append(skill.text.replace(" ",''))
 for loc in locations :
-    l.append(loc.text)
+    l.append(loc.text.replace(" ",''))
 print("_______________________________________________________________________________________________")
 for i in range (0,len(j)):
-    print(j[i]+s[i]+l[i])
+    print("Company name : "+j[i].strip())
+    print(s[i])
+    print(l[i])
     print("___________________________________________________________________________________________")
+
 
